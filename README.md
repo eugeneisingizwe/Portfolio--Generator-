644cd561f7857e1bc75a9e72bc108fb54d7d42f3
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2023-08-25 20:41:28 -0600

    updated inquirer version to work properly

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Portfolio--Generator-
+# Portfolio- Generator-
+
+## Description
+
+In this project, I am builing a command-line tool that generates an HTML portfolio page from user input.
+
+## Instructions
+
+* The application should prompt the user for information like their name, location, bio, LinkedIn URL, GitHub URL, etc
+
+* An HTML document containing the information collected from the prompts should be constructed and written to the file system. 
+
+## Installation
+
+* The application usus the following tools and technologies to accomplish this:
+
+  * `fs` for writing to the file system
+
+  * `inquirer` version 8.2.4 for collecting user input
+
+  * String template literals for generating a string version of the HTML document before it is written to the file system
+
+## Usage
+* Link to recorded video: https://drive.google.com/file/d/1y8AJbYK5VmKYFpwND1u0PpiFXmAOFqP-/view 
+
+![alt text](images/mock-up.png)
+
+---
+
+© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
--- index.js
+++ index.js
@@ -1,71 +1,81 @@
-const inquire = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-const generateHTML = ({ name, location, github, linkedIn }) =>
-`<!DOCTYPE html>
+const generateHTML = ({ name, location, github, linkedin, language }) =>
+  `<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio Generator</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
 </head>
 <body>
-    <div class="jumbotron jumbotron-fluid">
-     <div class="container">
-        <h1 class="display-4">My name is ${name}</h1>
-        <p class="lead">I am from ${location}</p>
-        <h3>Example heaing <span class="bagde badge-secondary">Contact Me</span></h3>
-        <lu class="lsit-group">
-        <li class="list-group-item">My Github username ${github} </li>
-        <li class="list-group-item">LinkedIn:${linkedIn} </li>
-        </lu>
-        </div>
-    </div>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${name}</h1>
+    <p class="lead">I am from ${location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${github}</li>
+      <li class="list-group-item">LinkedIn: ${linkedin}</li>
+      </ul>
+      <h3>What langauages do you know?</h3>
+      <form action="/action_page.php">
+      <input type="checkbox" id="langauge" name="langauge1" value="JavaScript">
+      <label for="langauge1"> I Know: ${language}</label><br>
+      <input type="submit" value="Submit">
+      </form>
+  </div>
+</div>
 </body>
 </html>`;
 
-inquire
-.prompt([
+inquirer
+  .prompt([
     {
-        type: "input",
-        name: "name",
-        message: "what is you name?",
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
     },
     {
-        type: "input",
-        name: "location",
-        message: "where you from?",
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
     },
     {
-        type: "input",
-        name: "food",
-        message: "what is your favorite food",
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
     },
     {
-        type: "input",
-        name: "hobby",
-        message: "what is your favorite hobby?",
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
     },
     {
-        type: "input",
-        name: "github",
-        message: "what is your github username?",
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
     },
     {
-        type: "input",
-        name: "linkedIn",
-        message: "what is your linkedIn username?",
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
     },
-    
-])
+    {
+        type: 'list',
+          message: 'What languages do you know?',
+          name: 'language',
+          choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+        },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
 
-.then((resposne) => {
-    const htmlPageContent = generateHTML(resposne);
- 
-    fs.writeFile("index.html", htmlPageContent, (err) => 
-    err ? console.log(err) : console.log("Successfully created index.html!")
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
     );
-});
+  });
 
 
